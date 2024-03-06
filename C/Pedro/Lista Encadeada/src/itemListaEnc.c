7c6558d9c43fb5b698e821c6886ac11c17eb89c0
Vttrium <phsauthier@yahoo.com>
2024-03-06 19:37:19 -0300

    J

--- C/Pedro/Lista Encadeada/src/itemListaEnc.c
+++ C/Pedro/Lista Encadeada/src/itemListaEnc.c
@@ -1,125 +1,141 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "itemListaEnc.h"
-
-typedef struct item Item;
-typedef struct celula Celula;
-typedef struct lista Lista;
+#include "../include/itemListaEnc.h"
 
 struct item {
-    int chave;
+  int chave;
+  //outros campos
 };
 
 struct celula {
-    Item item;
-    Celula *prox;
+  Item item;
+  Celula *prox;
 };
 
 struct lista {
-    Celula *primeira;
+  Celula *primeira;
 };
 
-Lista *criaListaVazia() {
-    Lista *lst = malloc(sizeof(Lista));
-    lst->primeira = NULL;
-    return lst;
+Lista* criaListaVazia() {
+  Lista *lst = malloc(sizeof(Lista));
+  lst->primeira = NULL;
+  return lst;
 }
 
 int verificaListaVazia(Lista *lst) {
-    return lst->primeira == NULL;
+  return lst->primeira == NULL;
 }
 
-Celula* buscaPorChave(Lista *lst, int chave) {
-    Celula *aux = lst->primeira;
-    while (aux != NULL && aux->item.chave != chave) {
-        aux = aux->prox;
-    }
-    return aux;
+// retorna o indice do item com a chave buscada ou -1 se não encontrar
+Celula* buscaPorChave(Lista * lst, int chave) {
+  Celula *aux = lst->primeira;
+  while (aux != NULL && aux->item.chave != chave) {
+    aux = aux->prox;
+  }
+
+  return aux;
 }
 
+// imprime a lista
 void imprimeLista(Lista *lst) {
-    Celula *aux;
-    for (aux = lst->primeira; aux != NULL; aux = aux->prox) {
-        printf("Chave: %d\n", aux->item.chave);
-    }
+  Celula *aux;
+  for (aux = lst->primeira; aux != NULL; aux = aux->prox) {
+    printf("Chave: %d\n", aux->item.chave);
+  }
 }
 
 // adiciona um elemento no inicio da lista
 void insereInicioLista(Lista *lst, int chave) {
-    Item novo;
-    novo.chave = chave;
-    Celula *nova = malloc(sizeof(Celula));
-    nova->item = novo;
-    nova->prox = lst->primeira;
-    lst->primeira = nova;
+  // Cria novo item que vai ser guardado na lista
+  Item novo;
+  novo.chave = chave;
+  // Cria nova célula que vai guardar o item
+  Celula *nova = malloc(sizeof(Celula));
+  nova->item = novo; // atribui ao campo item da nova célula o novo item
+  // inserção no início: a próxima célula é aquela que era a primeira
+  nova->prox = lst->primeira;
+  //a primeira agora é a nova célula
+  lst->primeira = nova;
 }
 
 // adiciona um elemento no fim da lista
 void insereFimLista(Lista *lst, int chave) {
-    Item novo;
-    novo.chave = chave;
-    Celula *nova = malloc(sizeof(Celula));
-    nova->item = novo;
-    nova->prox = NULL;
-    if (verificaListaVazia(lst)) {
-        lst->primeira = nova;
-    } else {
-        Celula *ultima;
-        ultima = lst->primeira; // partindo da primeira celula percorre 
-        while (ultima->prox != NULL) {
-            ultima = ultima->prox;
-        }
-        ultima->prox = nova; // apos a ultima inserir a nova celula
+  // Cria novo item que vai ser guardado na lista
+  Item novo;
+  novo.chave = chave;
+  // Cria nova célula que vai guardar o item
+  Celula *nova = malloc(sizeof(Celula));
+  nova->item = novo; // atribui ao campo item da nova célula o novo item
+  // inserção no fim da lista
+  nova->prox = NULL; // a nova célula vai ser a última, aponta para NULL
+  if (verificaListaVazia(lst)) { // se a lista está vazia, quem vai apontar para a nova célula é a primeira
+    lst->primeira = nova;
+  } else { // se não está vazia, quem vai apontar para a nova é a que era a última
+    Celula *ultima;
+    ultima = lst->primeira; // partindo da primeira célula percorre
+    while (ultima->prox != NULL) {
+      ultima = ultima->prox;
     }
+
+    ultima->prox = nova; // após a última, inserir a nova célula
+    
+  }
 }
 
 // adiciona um elemento no meio da lista
 void insereMeioLista(Lista *lst, int chave, int x) {
-    Celula *aux = buscaPorChave(lst, x); // procura pela chave x
-    if (aux != NULL) {
-        Item novo;
-        novo.chave = chave;
-        Celula *nova = malloc(sizeof(Celula));
-        nova->item = novo;
-        nova->prox = aux->prox;
-        aux->prox = nova;
-    } else {
-        printf("O item informado nao existe!");
-    }
+  Celula *aux = buscaPorChave(lst, x); // prcura pela chave x
+
+  if (aux != NULL) {
+    // Cria novo item que vai ser guardado na lista
+    Item novo;
+    novo.chave = chave;
+    // Cria nova célula que vai guardar o item
+    Celula *nova = malloc(sizeof(Celula));
+    nova->item = novo; // atribui ao campo item da nova célula o novo item
+    // Inserção no meio da lista
+    nova->prox = aux->prox;
+    aux->prox = nova;
+  } else {
+    printf("O item informado nao existe. \n");
+  }
 }
 
+// remove um item qualquer da lista
 void removeItem(Lista *lst, int x) {
-    Celula *aux = lst->primeira;
-    Celula *anterior;
+  Celula *aux = lst->primeira;
+  Celula *anterior;
+  // encontra o item com a chave que deve ser removida
+  while (aux != NULL && aux->item.chave != x) {
+    anterior = aux; // guarda quem é a célula anterior
+    aux = aux->prox;
+  }
 
-    while (aux != NULL && aux->item.chave != x) {
-        anterior = aux;
-        aux = aux->prox;
-    }
+  if (aux == NULL) {
+    printf("Item nao encontrado;");
+    return;
+  }
 
-    if (aux == NULL) {
-        printf("Item nao encontrado!");
-        return;
-    }
+  if (aux == lst->primeira) { // remover do começo
+    lst->primeira = aux->prox;
+  } else { // meio ou fim
+    anterior->prox = aux->prox;
+  }
 
-    if (aux == lst->primeira) {
-        lst->primeira = aux->prox;
-    } else {
-        anterior->prox = aux->prox;
-    }
-
-    free(aux);
+  free(aux);
 }
 
-//libera a memoria
-
+// libera da memória
 void liberaLista(Lista *lst) {
-    Celula *aux = lst->primeira;
-    Celula *liberar;
-    while (aux != NULL) {
-        liberar = aux;
-        aux = aux->prox;
-        free(liberar);
-    }
-    free(lst);
+  Celula *aux = lst->primeira;
+  Celula *liberar;
+  
+  while (aux != NULL) {
+    liberar = aux;
+    aux = aux->prox;
+    free(liberar);
+  }
+
+  free(lst);
+  
 }
--- C/Pedro/Lista Encadeada/src/main.c
+++ C/Pedro/Lista Encadeada/src/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "itemListaEnc.h"
+#include "../include/itemListaEnc.h"
 
 int main() {
     Lista *l;
